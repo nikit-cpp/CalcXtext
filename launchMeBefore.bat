ff4322314686c4869b0d4cde8a4761d02634d7f9
nikit.cpp <nikit.cpp@yandex.ru>
2013-10-17 21:21:12 +0300

    added preparuing script launchMeBefore.bat

---
+++ launchMeBefore.bat
@@ -0,0 +1,15 @@
+@echo off
+
+md .\org.eclipse.xtext.example.arithmetics\src-gen
+md .\org.eclipse.xtext.example.arithmetics\xtend-gen
+
+md .\org.eclipse.xtext.example.arithmetics.tests\src-gen
+md .\org.eclipse.xtext.example.arithmetics.tests\xtend-gen
+
+md .\org.eclipse.xtext.example.arithmetics.ui\src-gen
+md .\org.eclipse.xtext.example.arithmetics.ui\xtend-gen
+
+
+echo now launch eclipse, clean projects and
+echo ignoring errors launch .mwe2
+pause
